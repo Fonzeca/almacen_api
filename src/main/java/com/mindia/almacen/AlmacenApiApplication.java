fe886c27e0940e64b4730f658cde42eefa4e1028
Fragore <alexisfonzos_@hotmail.com>
2021-04-29 20:33:28 -0300

    Try to work inside of tomcat

--- src/main/java/com/mindia/almacen/AlmacenApiApplication.java
+++ src/main/java/com/mindia/almacen/AlmacenApiApplication.java
@@ -2,10 +2,17 @@ package com.mindia.almacen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AlmacenApiApplication {
-
+public class AlmacenApiApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AlmacenApiApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AlmacenApiApplication.class, args);
 	}
